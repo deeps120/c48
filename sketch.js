6b3e55923ca20552b0d42c4590aa2c106b21ed49
deeps120 <62687789+deeps120@users.noreply.github.com>
2020-06-04 20:37:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function preload(){
   coinImage=loadImage("download.png");
 }
 function setup() {
- createCanvas(displayWidth-30,displayHeight-70);
+ createCanvas(displayWidth,displayHeight);
   ground= createSprite(displayWidth/2,displayHeight/2,displayWidth*3,displayHeight*4);
   ground.addImage("ground",groundImage);
   ground.scale=3;
@@ -31,7 +31,6 @@ function setup() {
    cometGroup=createGroup();
    blocksGroup=createGroup();
    coinGroup=createGroup();
-  // player.debug=true;
    player.setCollider("rectangle",0,0,100,150);
 
     text1 =createElement('h2');
